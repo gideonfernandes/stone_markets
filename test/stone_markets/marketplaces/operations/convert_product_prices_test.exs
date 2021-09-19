56b32efbe6f8cea7258fa3bcbcac5a2e0cc75fe1
Gideon Fernandes <gideon.de.fernandes@gmail.com>
2021-09-19 15:08:28 -0300

    chore: add better consistence to the convert_product_prices_test

--- test/stone_markets/marketplaces/operations/convert_product_prices_test.exs
+++ test/stone_markets/marketplaces/operations/convert_product_prices_test.exs
@@ -4,7 +4,7 @@ defmodule StoneMarkets.Marketplaces.Operations.ConvertProductPricesTest do
   import Mox
   import StoneMarkets.Factory
 
-  alias StoneMarkets.{Arithmetic, Product}
+  alias StoneMarkets.{Arithmetic, Product, Repo}
   alias StoneMarkets.ExchangerateApi.ClientMock
   alias StoneMarkets.Marketplaces.Operations.ConvertProductPrices
 
@@ -19,14 +19,17 @@ defmodule StoneMarkets.Marketplaces.Operations.ConvertProductPricesTest do
       old_price2 = Arithmetic.cast!(200, :convert).value
       old_price3 = Arithmetic.cast!(300, :convert).value
 
-      insert(:product, marketplace: marketplace, shopkeeper: shopkeeper, price: old_price1)
-      insert(:product, marketplace: marketplace, shopkeeper: shopkeeper, price: old_price2)
-      insert(:product, marketplace: marketplace, shopkeeper: shopkeeper, price: old_price3)
+      product_ids = [
+        insert(:product, marketplace: marketplace, shopkeeper: shopkeeper, price: old_price1).id,
+        insert(:product, marketplace: marketplace, shopkeeper: shopkeeper, price: old_price2).id,
+        insert(:product, marketplace: marketplace, shopkeeper: shopkeeper, price: old_price3).id
+      ]
 
-      {:ok, marketplace: marketplace, old_currency_id: old_currency_id}
+      {:ok, marketplace: marketplace, old_currency_id: old_currency_id, product_ids: product_ids}
     end
 
     test "updates all product's price when the market currency changed", %{
+      product_ids: product_ids,
       marketplace: marketplace,
       old_currency_id: old_currency_id
     } do
@@ -39,7 +42,7 @@ defmodule StoneMarkets.Marketplaces.Operations.ConvertProductPricesTest do
 
       ConvertProductPrices.call({marketplace, old_currency_id})
 
-      response = Repo.all(Product)
+      response = Enum.map(product_ids, &Repo.get!(Product, &1))
 
       assert [
                %Product{id: _, price: 1_898_999},
