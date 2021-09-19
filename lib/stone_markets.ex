14752e4ad5854b2a6cb6c813021748bb176b4307
Gideon Fernandes <gideon.de.fernandes@gmail.com>
2021-09-19 03:10:29 -0300

    feat: add arithmetic struct & facade pattern on business logic

--- lib/stone_markets.ex
+++ lib/stone_markets.ex
@@ -6,4 +6,44 @@ defmodule StoneMarkets do
   Contexts are also responsible for managing your data, regardless
   if it comes from the database, an external API or others.
   """
+
+  alias StoneMarkets.Accounts.Deposit, as: AccountDeposit
+  alias StoneMarkets.Currencies.Index, as: IndexCurrencies
+  alias StoneMarkets.Currencies.Fetch, as: FetchCurrency
+  alias StoneMarkets.Currencies.FetchBy, as: FetchCurrencyBy
+  alias StoneMarkets.Customers.Create, as: CreateCustomer
+  alias StoneMarkets.Customers.Index, as: IndexCustomers
+  alias StoneMarkets.Customers.Fetch, as: FetchCustomer
+  alias StoneMarkets.Customers.FetchBy, as: FetchCustomerBy
+  alias StoneMarkets.Orders.Create, as: CreateOrder
+  alias StoneMarkets.Marketplaces.Create, as: CreateMarketplace
+  alias StoneMarkets.Marketplaces.Index, as: IndexMarketplaces
+  alias StoneMarkets.Marketplaces.FetchBy, as: FetchMarketplaceBy
+  alias StoneMarkets.Marketplaces.Update, as: UpdateMarketplace
+  alias StoneMarkets.Products.Create, as: CreateProduct
+  alias StoneMarkets.Products.Index, as: IndexProducts
+  alias StoneMarkets.Products.Fetch, as: FetchProduct
+  alias StoneMarkets.Shopkeepers.Create, as: CreateShopkeeper
+  alias StoneMarkets.Shopkeepers.Index, as: IndexShopkeepers
+  alias StoneMarkets.Shopkeepers.Fetch, as: FetchShopkeeper
+
+  defdelegate account_deposit(params, account_type), to: AccountDeposit, as: :call
+  defdelegate currencies, to: IndexCurrencies, as: :call
+  defdelegate fetch_currency(id), to: FetchCurrency, as: :call
+  defdelegate fetch_currency_by(field, param), to: FetchCurrencyBy, as: :call
+  defdelegate create_customer(params), to: CreateCustomer, as: :call
+  defdelegate fetch_customer(id), to: FetchCustomer, as: :call
+  defdelegate fetch_customer_by(field, param), to: FetchCustomerBy, as: :call
+  defdelegate customers, to: IndexCustomers, as: :call
+  defdelegate create_order(params), to: CreateOrder, as: :call
+  defdelegate create_marketplace(params), to: CreateMarketplace, as: :call
+  defdelegate marketplaces, to: IndexMarketplaces, as: :call
+  defdelegate fetch_marketplace_by(field, param), to: FetchMarketplaceBy, as: :call
+  defdelegate update_marketplace(params), to: UpdateMarketplace, as: :call
+  defdelegate create_product(params), to: CreateProduct, as: :call
+  defdelegate fetch_product(id), to: FetchProduct, as: :call
+  defdelegate products(params), to: IndexProducts, as: :call
+  defdelegate create_shopkeeper(params), to: CreateShopkeeper, as: :call
+  defdelegate fetch_shopkeeper(id), to: FetchShopkeeper, as: :call
+  defdelegate shopkeepers(params), to: IndexShopkeepers, as: :call
 end
---
+++ lib/stone_markets/arithmetic.ex
@@ -0,0 +1,64 @@
+defmodule StoneMarkets.Arithmetic do
+  @enforce_keys ~w(conversor original precision value)a
+
+  defstruct @enforce_keys
+
+  alias StoneMarkets.Errors.{InvalidArgs, IsNotANumber}
+
+  def build(value) when is_number(value) do
+    %__MODULE__{
+      conversor: nil,
+      original: value,
+      precision: nil,
+      value: value
+    }
+  end
+
+  def build(_), do: {:error, IsNotANumber.call()}
+
+  def cast(value, :convert) when is_number(value) do
+    {:ok,
+     %__MODULE__{
+       conversor: 100_000,
+       original: value,
+       precision: 5,
+       value: trunc(value * 100_000)
+     }}
+  end
+
+  def cast(value, :deconvert) when is_number(value) do
+    {:ok,
+     %__MODULE__{
+       conversor: 100_000,
+       original: value,
+       precision: 5,
+       value: floatify(value / 100_000, 5)
+     }}
+  end
+
+  def cast(_, _), do: {:error, InvalidArgs.call()}
+
+  def cast!(value, :convert) when is_number(value) do
+    %__MODULE__{
+      conversor: 100_000,
+      original: value,
+      precision: 5,
+      value: trunc(value * 100_000)
+    }
+  end
+
+  def cast!(value, :deconvert) when is_number(value) do
+    %__MODULE__{
+      conversor: 100_000,
+      original: value,
+      precision: 5,
+      value: floatify(value / 100_000, 5)
+    }
+  end
+
+  defp floatify(value, precision) do
+    value
+    |> :erlang.float_to_binary(decimals: precision)
+    |> String.to_float()
+  end
+end
